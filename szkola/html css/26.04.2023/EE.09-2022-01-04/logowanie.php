a16b6bdc88c4b015169dc7b474b804cfea853e48
adrix5521 <adrianborowski16@gmail.com>
2023-04-26 16:44:48 +0200

    Update 26.04.2023
    Duzo zadan z HTML i CSS tak o bo mi sie nudzilo a lubie

---
+++ szkola/html css/26.04.2023/EE.09-2022-01-04/logowanie.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="./styl4.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section id="baner">
+        <h1>Forum wielbicieli psów</h1>
+    </section>
+    <section id="lewy">
+        <img src="./obraz.jpg" alt="foksterier">
+    </section>
+    <section id="prawy1">
+        <h2>Zapisz się</h2>
+        <form action="./logowanie.php" method="post">
+            login: <input type="text"> <br>
+            hasło: <input type="password"> <br>
+            powtórz hasło: <input type="password"> <br>
+            <button type="submit">Zapisz</button>
+        </form>
+    </section>
+    <section id="prawy2">
+        <h2>Zapraszamy wszystkich</h2>
+        <ol type="1">
+            <li>właścicieli psów</li>
+            <li>weterynarzy</li>
+            <li>tych, co chcą kupić psa</li>
+            <li>tych, co lubią psy</li>
+        </ol>
+        <a href="./regulamin.html">Przeczytaj regulamin forum</a>
+    </section>
+    <section id="stopka">
+        Stronę wykonał: Adrian Borowski
+    </section>
+</body>
+</html>
---
+++ szkola/html css/26.04.2023/INF.03-01-22.06-SG/wedkuj.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="./styl_1.css">
+</head>
+<body>
+    <section id="baner">
+        <h1>Portal dla wędkarzy</h1>
+    </section>
+    <section id="lewy1">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol type="1">
+            <li>Szczupak pływa w rzece Warta-Obrzycko, Wielkopolskie</li>
+            <li>Leszcz pływa w rzece Przemsza k. Okradzinowa, Śląskie</li>
+        </ol>
+    </section>
+    <section id="prawy">
+        <img src="./ryba1.jpg" alt="Sum"> <br>
+        <a href="./kwerendy.txt">Pobierz kwerendy</a>
+    </section>
+    <section id="lewy2">
+        <h3>Ryby drapieżne naszych wód</h3>
+        <table>
+            <tr>
+                <th>L.p.</th>
+                <th>Gatunek</th>
+                <th>Występowanie</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Szczupak</td>
+                <td>stawy, rzeki</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Sandacz</td>
+                <td>stawy, jeziora, rzeki</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Okoń</td>
+                <td>rzeki</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Sum</td>
+                <td>jeziora, rzeki</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Dorsz</td>
+                <td>morza, oceany</td>
+            </tr>
+        </table>
+    </section>
+    <section id="stopka">
+        <p>Stronę wykonał: Adrian Borowski</p>
+    </section>
+</body>
+</html>
